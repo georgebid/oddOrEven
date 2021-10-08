07a7c8b982679a48c7bef8ae7f3f8f36d47abf8a
georgebid <91198628+georgebid@users.noreply.github.com>
2021-10-08 16:17:05 +0100

    Add project files.

---
+++ I'mAnAlien/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace I_mAnAlien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Multiply(3, 7);
+            Console.WriteLine($"The result is {result}");
+
+            if (result % 2 == 0)
+            {
+                Console.WriteLine($"The result is an even number!");
+            } else
+            {
+                Console.WriteLine($"The result is an odd number!");
+            }
+
+        }
+        static int Multiply(int num01, int num02)
+        {
+            int result = num01 * num02;
+            return result;
+        }
+    }
+}
